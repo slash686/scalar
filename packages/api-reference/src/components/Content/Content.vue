06e308e3505b55fb0321eae9ac3466523cade616
Amrit <amrit@scalar.com>
2024-11-19 16:00:53 -0800

    feat: wicked progress

--- packages/api-reference/src/components/Content/Content.vue
+++ packages/api-reference/src/components/Content/Content.vue
@@ -69,7 +69,9 @@ const introCardsSlot = computed(() =>
           <RequestAuth
             v-if="activeCollection"
             :collection="activeCollection"
-            :selectedSecuritySchemeUids="[]"
+            :selectedSecuritySchemeUids="
+              activeCollection.selectedSecuritySchemeUids
+            "
             title="Authentication" />
           <ClientLibraries class="introduction-card-item" />
         </div>
